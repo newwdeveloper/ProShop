b5845852e18cea7bfc437275e1c8a1ab45a3e368
Akshay <akshaymangutkar951@gmail.com>
2025-03-10 16:07:42 +0530

    feat: Implement dynamic routing with react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-function App() {
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
+
+const AppLayout = () => {
   return (
-    <>
-      <h1>Welcome to the Shop</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow py-3">
+        <div className="max-w-6xl mx-auto px-4">
+          <Outlet /> {/* This will render the matched route */}
+        </div>
+      </main>
+      <Footer className="mt-auto" />
+    </div>
   );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />, // Main layout
+    children: [
+      { element: <HomeScreen />, index: true }, // Nested route
+      { path: "product/:id", element: <ProductScreen /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
